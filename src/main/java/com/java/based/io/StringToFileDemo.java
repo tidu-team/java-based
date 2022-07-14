efada0a7bf7a144fdefb117e2fb1d53a551630b8
honglixiang@tiduyun.com <honglixiang@tiduyun.com>
2022-07-14 17:03:12 +0800

    feat: 补充提交

---
+++ src/main/java/com/java/based/io/StringToFileDemo.java
@@ -0,0 +1,57 @@
+package com.java.based.io;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author honglixiang@tiduyun.com
+ * @date 2022/7/14
+ */
+public class StringToFileDemo {
+
+    public static void main(String[] args) throws IOException{
+        String file = "strTo.text";
+        String s ="132 456789";
+//        method(file,s);
+//        method2(file,s);
+//        method3(file,s);
+//        method4(file,s);
+        method5(file,s);
+    }
+
+
+    public static void method(String file,String context) throws IOException {
+        FileOutputStream out = new FileOutputStream(file);
+        out.write(context.getBytes(StandardCharsets.UTF_8));
+        out.close();
+    }
+
+    public static void method2(String file,String context) throws IOException {
+        BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(file));
+        out.write(context.getBytes(StandardCharsets.UTF_8));
+        out.close();
+    }
+
+    public static void method3(String file,String context) throws IOException {
+        FileWriter out = new FileWriter(file);
+        out.write(context);
+        out.close();
+    }
+
+    public static void method4(String file,String context) throws IOException {
+        BufferedWriter out = new BufferedWriter(new FileWriter(file));
+        out.write(context);
+        out.close();
+    }
+
+    public static void method5(String file,String context) throws IOException {
+        PrintWriter out = new PrintWriter(new FileWriter(file));
+        out.write(context);
+        out.close();
+    }
+}
